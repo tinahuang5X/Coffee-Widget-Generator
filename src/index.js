e5ba146def0932287595dfb3eccfbad9bb9d55bb
Tina Huang <tingyuhuang@aol.com>
2017-08-09 14:47:34 -0700

    completed generator

--- src/index.js
+++ src/index.js
@@ -1,7 +1,68 @@
-addEventListener('DOMContentLoaded', main);
+/*addEventListener('DOMContentLoaded', main);
 
 function main() {
   const $root = document.querySelector('#root');
   const $app = null; // <== Your dynamically generated DOM element goes here
   $root.appendChild($app);
+}
+*/
+addEventListener('DOMContentLoaded', main);
+const CoffeePageScraper = require('./scrapers/CoffeePageScraper.js');
+const {
+  div,
+  h1,
+  h2,
+  h3,
+  h4,
+  span,
+  button,
+  img,
+  form,
+  input,
+  label
+} = require('elementx');
+
+function main() {
+  const $root = document.querySelector('#root');
+  const $searchForm = div(
+    { class: 'coffee search' },
+    label(
+      { id: 'label' },
+      `Please enter the web address of the coffee you are looking for.`
+    ),
+    form(
+      { id: 'form' },
+      input({ id: 'input', type: 'url' }),
+      button({ type: 'submit', id: 'button' }, 'Submit')
+    )
+  );
+  // const $app = $searchForm; // <== Your dynamically generated DOM element goes here
+  // $root.appendChild($app);
+  $root.appendChild($searchForm);
+
+  const coffeeObject = new CoffeePageScraper();
+  let $button = document.getElementById('button');
+  $button.addEventListener('click', function() {
+    event.preventDefault();
+    let webAddress = document.getElementById('input');
+    //console.log(webAddress.value);
+    if (webAddress.value.length > 0) {
+      coffeeObject.scrape(webAddress.value).then(responseObj => {
+        const $widget = div({ class: 'coffee widget' }, [
+          img({ src: responseObj.image }),
+          h1(responseObj.brand),
+          h2(responseObj.name),
+          h3(responseObj.description),
+          h3(responseObj.boxcount, ' - ', responseObj.price),
+          h4('Customer Review: ', responseObj.review)
+        ]);
+        //console.log($widget);
+        const $app = $widget;
+        $root.appendChild($app);
+      });
+    } else {
+      alert('Please enter a valid web address!');
+      return;
+    }
+  });
 }
--- src/scrapers/CoffeePageScraper.js
+++ src/scrapers/CoffeePageScraper.js
@@ -1,32 +1,45 @@
 class CoffeePageScraper {
-  constructor(url) {
-    this.url = url;
+  // TODO: DELETE THIS CONSTRUCTOR, NOT NEEDED
+  // constructor() {
+  //   this.image = doc.getElementById('base_image').src;
+  //   this.brand = doc.getElementsByClassName('kold medium-4 columns')[0].children[0].innerText;
+  //   this.coffeename = doc.getElementsByClassName('productHeading')[0].innerText;
+  //   this.listArray;
+  // }
+
+  scrape(url) {
+    return fetch(url).then(response => response.text()).then(html => {
+      let parser = new DOMParser();
+      let doc = parser.parseFromString(html, 'text/html');
+      this.image = doc.getElementById('base_image');
+      //this.imageLink = doc.getElementById('base_image').src;
+      this.brand = doc.getElementsByClassName('kold medium-4 columns');
+      this.coffeename = doc.getElementsByClassName('productHeading');
+      this.list = doc.getElementsByClassName('brewerPdpWrapperComponentRight');
+      this.listArray = [];
+      for (let i = 0; i < this.list[0].children[0].children.length; i++) {
+        this.listArray.push(this.list[0].children[0].children[i].innerText);
       }
+      this.option = doc.getElementsByTagName('option');
+      this.review = doc.getElementsByClassName('bvseo-review');
+      //console.log(brand[0].children[0].innerText);
+      //console.log(coffeename[0].innerText);
+      //console.log(listArray.join());
+      //console.log(option[1].getAttribute("data-info"));
+      //console.log(option[1].getAttribute("data-price"));
+      //console.log(review[0].children[4].innerText);
+      let obj = {
+        image: this.image.src,
+        brand: this.brand[0].children[0].innerText,
+        name: this.coffeename[0].innerText,
+        description: this.listArray.join('. '),
+        boxcount: this.option[1].getAttribute('data-info'),
+        price: this.option[1].getAttribute('data-price'),
+        review: this.review[0].children[4].innerText
+      };
+      return obj;
+    });
+  }
+}
 
-scrape(url) {
-  fetch('http://cors-bypass-proxy.axiomlogic.com/http://www.keurig.com/Beverages/Coffee/Regular/Dark-Magic%C2%AE-Extra-Bold-Coffee/p/Dark-Magic-Extra-Bold-Coffee-K-Cup-Green-Mountain/')
-  .then(response => response.text())
-  .then(html =>  {
-  let parser = new DOMParser();
-  let doc = parser.parseFromString(html, "text/html");
-  let image = doc.getElementById('base_image');
-  let content = doc.getElementsByClassName('productHeading');
-  var title = doc.getElementsByClassName('kold medium-4 columns');
-  let list = doc.getElementsByClassName('brewerPdpWrapperComponentRight');
-  //console.log(title[0].children[0].innerText);
-  console.log(list[0].children[0].children[0].innerHTML);
-  return doc;
-})
-
-
-const { div, h1, a } = require('elementx')
-
-const node = div(
-  h1({ class: 'title' }, title),
-  div({ class: 'bg-red' },
-    a({ href: 'http://github.com' }, 'Github')
-  )
-)
-
-
-document.body.appendChild(node)
+module.exports = CoffeePageScraper;
